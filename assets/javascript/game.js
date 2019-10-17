d971e9cb3eae9030999d489d66f972c436e24a4c
Aliesa Jackson <aliesaj2010@yahoo.com>
2019-10-16 21:03:03 -0400

    added pseudo code & html code

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+//1 get user input 
+// 1 How to get user input?
+
+//2 Get the letter that was chosen
+
+//3 Record if it was a win or loss - how many # of times they guessed correctly or incorrectly
+
+//4 Record how many guesses they have left after each attempt
+
+//5 Record what letters were guessed - specific letters typed by user- display until game over
+
+//6 when the player wins increase the wins counter
+
+//7 when player loses increase the loser counter
+
+//8 restart game after win or loss without page refresh
+
+
+
+// this function is ran whenever user presses a key
+document.onkeyup = function(event){
+    // variable created determines what key is pressed
+    var userGuest = event.key;
+}
+
+
+
