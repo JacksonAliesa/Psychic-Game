eef957e39007f28bd2181fb614ed73dc9a513f31
Aliesa Jackson <aliesaj2010@yahoo.com>
2019-10-20 18:53:33 -0400

    populated the starting screen to display # of guesses/wins/losses before the start of game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -25,10 +25,20 @@ var totalWins = document.getElementById('totalWins');
 var totalLosses = document.getElementById('totalLosses');
 var guessLeft = document.getElementById('guessLeft');
 var guessTracker = document.getElementById('guessTracker');
+
+totalWins.textContent = 'Wins: ' + wins;
+totalLosses.textContent = 'Losses: ' + losses;
+guessLeft.textContent = 'Guesses Left:' + guess;
+
 // this function is ran whenever user presses a key
 document.onkeyup = function(event) {
 	// variable created determines what key is pressed
 	var userGuess = event.key;
+
+	// variables that we want to appear on web browser from the html file
+	//this will display
+
+	guessTracker.textContent = 'Your Guesses So Far:' + userGuess;
 
 	var letterChoices = [
 		'a',
@@ -64,21 +74,21 @@ document.onkeyup = function(event) {
 	// got to reset the game when the guesses run out for user
 	// attempted to use the push method but it was unsuccessful thus far
 	if (userGuess !== compGuess) {
-		console.log(guess--);
-		console.log(losses++);
+		guess--;
+		losses++;
 		if (guess == 0) {
-			console.log((guess = 9));
-			console.log((losses = 0));
-			console.log((wins = 0));
+			guess = 9;
+			losses = 0;
+			wins = 0;
 		}
 	} else if (userGuess === compGuess) {
-		console.log(wins++);
-		console.log(guess--);
+		wins++;
+		guess--;
 
 		if (guess == 0) {
-			console.log((guess = 9));
-			console.log((losses = 0));
-			console.log((wins = 0));
+			guess = 9;
+			losses = 0;
+			wins = 0;
 		}
 	}
 
@@ -86,8 +96,8 @@ document.onkeyup = function(event) {
 	//this will display
 	totalWins.textContent = 'Wins: ' + wins;
 	totalLosses.textContent = 'Losses: ' + losses;
-	guessTracker.textContent = 'Your Guesses So Far:' + userGuess;
 	guessLeft.textContent = 'Guesses Left:' + guess;
+	guessTracker.textContent = 'Your Guesses So Far:' + userGuess;
 
-	console.log(guessArr.push(userGuess));
+	console.log(guessArr.join(userGuess));
 };
